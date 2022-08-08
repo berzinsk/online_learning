3dad7ffd474042157aa26fd787e9e32ade14d65d
Karlis Berzins <berzinsh.karlis@gmail.com>
2022-08-08 14:00:09 +0300

    Update navbar to be fixed

--- lib/navigation/bottom_nav_bar.dart
+++ lib/navigation/bottom_nav_bar.dart
@@ -15,39 +15,45 @@ class _BottomNavBarState extends State<BottomNavBar> {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: _selectedIndex,
-      onTap: _onItemTapped,
-      elevation: 0,
-      showUnselectedLabels: true,
-      selectedFontSize: 12,
-      selectedItemColor: AppColors.kPrimaryBlue,
-      selectedLabelStyle: GoogleFonts.poppins(),
-      unselectedFontSize: 12,
-      unselectedItemColor: AppColors.kMenuGray,
-      unselectedLabelStyle: GoogleFonts.poppins(),
-      items: [
-        BottomNavigationBarItem(
-          icon: Image.asset('asset/images/icon_nav_home.png'),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: Image.asset('asset/images/icon_nav_course.png'),
-          label: 'Course',
-        ),
-        BottomNavigationBarItem(
-          icon: Image.asset('asset/images/icon_nav_search.png'),
-          label: 'Search',
-        ),
-        BottomNavigationBarItem(
-          icon: Image.asset('asset/images/icon_nav_notification.png'),
-          label: 'Message',
-        ),
-        BottomNavigationBarItem(
-          icon: Image.asset('asset/images/icon_nav_account.png'),
-          label: 'Account',
-        ),
-      ],
+    return Theme(
+      data: ThemeData(
+        splashFactory: NoSplash.splashFactory,
+      ),
+      child: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        elevation: 0,
+        showUnselectedLabels: true,
+        selectedFontSize: 12,
+        selectedItemColor: AppColors.kPrimaryBlue,
+        selectedLabelStyle: GoogleFonts.poppins(),
+        unselectedFontSize: 12,
+        unselectedItemColor: AppColors.kMenuGray,
+        unselectedLabelStyle: GoogleFonts.poppins(),
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset('asset/images/icon_nav_home.png'),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('asset/images/icon_nav_course.png'),
+            label: 'Course',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('asset/images/icon_nav_search.png'),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('asset/images/icon_nav_notification.png'),
+            label: 'Message',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('asset/images/icon_nav_account.png'),
+            label: 'Account',
+          ),
+        ],
+      ),
     );
   }
 
