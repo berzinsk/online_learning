875667e63911d9658d862a1189e3c5954612af2a
Karlis Berzins <berzinsh.karlis@gmail.com>
2022-08-18 16:11:08 +0300

    Move course widgets to correct folders

--- lib/screens/courses.dart
+++ lib/screens/courses.dart
@@ -3,6 +3,7 @@ import 'package:online_learning/resources/constants/colors.dart';
 
 import '../model/course.dart';
 import '../resources/data/demo_courses.dart';
+import '../components/course/course_filter_row.dart';
 
 class Courses extends StatefulWidget {
   const Courses({Key? key}) : super(key: key);
@@ -73,91 +74,6 @@ class _CoursesState extends State<Courses> {
               onFilterSelected: onFilterSelected,
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class CourseFilterRow extends StatelessWidget {
-  final int selectedFilterIndex;
-  final void Function(int index) onFilterSelected;
-
-  const CourseFilterRow({
-    Key? key,
-    required this.selectedFilterIndex,
-    required this.onFilterSelected,
-  }) : super(key: key);
-
-  void onCourseFilterItemPress(int index) {
-    onFilterSelected(index);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        CourseFilterRowItem(
-          buttonIndex: 0,
-          title: 'All',
-          onButtonPress: onCourseFilterItemPress,
-          isSelected: selectedFilterIndex == 0,
-        ),
-        CourseFilterRowItem(
-          title: 'Popular',
-          onButtonPress: onCourseFilterItemPress,
-          buttonIndex: 1,
-          isSelected: selectedFilterIndex == 1,
-        ),
-        CourseFilterRowItem(
-          title: 'New',
-          onButtonPress: onCourseFilterItemPress,
-          buttonIndex: 2,
-          isSelected: selectedFilterIndex == 2,
-        ),
-        const SizedBox(width: 40),
-      ],
-    );
-  }
-}
-
-class CourseFilterRowItem extends StatelessWidget {
-  final bool isSelected;
-  final int buttonIndex;
-  final String title;
-  final void Function(int index) onButtonPress;
-
-  const CourseFilterRowItem({
-    Key? key,
-    required this.buttonIndex,
-    required this.title,
-    required this.onButtonPress,
-    this.isSelected = false,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 76,
-      child: OutlinedButton(
-        style: OutlinedButton.styleFrom(
-          padding: EdgeInsets.zero,
-          backgroundColor:
-              isSelected ? AppColors.kPrimaryBlue : Colors.transparent,
-          splashFactory: NoSplash.splashFactory,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(32),
-          ),
-          side: const BorderSide(color: Colors.transparent),
-        ),
-        onPressed: () {
-          onButtonPress(buttonIndex);
-        },
-        child: Text(
-          title,
-          style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-              color: isSelected ? Colors.white : AppColors.kPrimaryTextGray),
         ),
       ),
     );
